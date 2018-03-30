fc810ef94507bd64d9e65f222f463591f728b080
LAPTOP-LHP44F0Q\Alessandra <alessandragenca@gmail.com>
2018-03-30 10:25:38 +0200

    Prova caricamento classi di test delle risorse Json

---
+++ NoteManager/NoteManager/src/main/java/it/libero/alessandragenca/notesmanager/server/backend/UserRegistry.java
@@ -0,0 +1,108 @@
+package it.libero.alessandragenca.notesmanager.server.backend;
+
+import it.libero.alessandragenca.notesmanager.commons.InvalidUsernameException;
+import it.libero.alessandragenca.notesmanager.commons.User1;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.type.TypeReference;
+import org.restlet.security.MemoryRealm;
+import org.restlet.security.User;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.ArrayList;
+
+
+
+public class UserRegistry {
+	
+	public UserRegistry(){
+		
+		realm= new MemoryRealm();
+	}
+	
+	public int size (){
+		return realm.getUsers().size();
+	}
+	
+	public User get (String username) throws InvalidUsernameException{
+		User u= realm.findUser(username);
+		if (u!=null)return u;
+		throw new InvalidUsernameException("Username non valido: " + username);
+			
+		
+	}
+	
+	public void add (User u) throws InvalidUsernameException{
+		//controlliamo se c'e' gia un user con quell'identifier
+		for (User users: realm.getUsers()){
+			if(u.getIdentifier().equalsIgnoreCase(users.getIdentifier()))
+				throw new InvalidUsernameException("Username Duplicato:" + u.getIdentifier());
+			}
+		realm.getUsers().add(u);
+
+	}
+	
+	public void update(User u){
+		realm.getUsers().add(u);
+		}
+	
+	public void remove (User u) throws InvalidUsernameException{
+		//for (User users: realm.getUsers()){
+			//if(u.getIdentifier().equalsIgnoreCase(users.getIdentifier()))
+				//realm.getUsers().remove(u);
+		User u1= realm.findUser(u.getIdentifier());
+		if (u1!=null) realm.getUsers().remove(u);
+		else throw new InvalidUsernameException("Username non presente, non posso rimuovere l'utente: " + u.getIdentifier());
+
+
+
+	}
+
+		
+	public void save(String fileOutName) throws IOException{		
+	    FileOutputStream fileOut = new FileOutputStream(fileOutName);
+	    PrintStream out = new PrintStream(fileOut);
+	   
+	    ArrayList<User1> userList = new ArrayList<User1>();
+	    ObjectMapper objectMapper = new ObjectMapper(); // Classe della libreria JACKSON per la memorizzazione della lista di utenti con codifica JSON
+	    for(User user : realm.getUsers())
+	    {
+	    	
+	    		
+	    		userList.add(new User1(user.getIdentifier(), user.getSecret()));
+	    }
+	    String json = objectMapper.writeValueAsString(userList); // Ottengo la stringa (con codifica JSON) rappresentante la lista di utenti
+	    out.print(json); // Scrivo su file (con codifica JSON) l'intera lista di utenti
+	    out.close();
+	    fileOut.close();
+	}
+	
+	// Caricamento della lista utenti da file 
+	
+	public void load(String fileName) throws IOException, ClassNotFoundException{
+	    FileInputStream fileIn = new FileInputStream(fileName);
+	    ArrayList<User1> userList = new ArrayList<User1>();
+	    ObjectMapper mapper = new ObjectMapper();
+	    // Tramite la funzione readValue riottengo la lista degli utenti a partire dalla stringa con codifica JSON
+	    userList = mapper.readValue(fileIn, new TypeReference<ArrayList<User1>>() {});
+	    for(User1 userWrapped : userList) // Per ogni utente nella lista, riottengo l'oggetto User, andando a ripopolare il Realm (ripristino)
+	    {
+	    	User user = new User(userWrapped.getIdentifier(), userWrapped.getSecret());
+	    	realm.getUsers().add(user); // Aggiungo l'utente alla lista
+	     }
+	    fileIn.close();
+	} 
+	
+	
+	
+	public MemoryRealm getRealm(){
+		return realm;
+	}
+	
+	
+	private static MemoryRealm realm;
+	
+}
+
---
+++ NoteManager/NoteManager/src/main/java/it/libero/alessandragenca/notesmanager/server/backend/tester/Test1.java
@@ -0,0 +1,156 @@
+package it.libero.alessandragenca.notesmanager.server.backend.tester;
+
+import it.libero.alessandragenca.notesmanager.commons.InvalidKeyException;
+import it.libero.alessandragenca.notesmanager.commons.Note;
+import it.libero.alessandragenca.notesmanager.server.backend.NoteRegistry;
+
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+public class Test1 {
+	
+
+
+	public static void main(String[] args) throws InvalidKeyException {
+
+		NoteRegistry nr = new NoteRegistry();
+				
+		System.out.println("Size: " + nr.size());
+		
+		//String myDateStr="09/11/17";
+		//Date myDate=null;
+		Date myDate =new Date();
+		    /*try {
+		     DateFormat dateFormat = DateFormat.getDateInstance(DateFormat.SHORT, Locale.ITALY);
+		     myDate = dateFormat.parse(myDateStr);
+		     //System.out.println(dateFormat.format(myDate));
+		    } catch (ParseException e) {
+		      e.printStackTrace();
+		    }*/
+		
+		try {
+			System.out.println("add-01");
+			nr.add(new Note("Nota1", "Questo � il testo della nota 1",myDate));
+			System.out.println("OK");
+		} catch (InvalidKeyException e) {
+			System.out.println("ERR: " + e.getMessage());
+		}
+		
+		try {
+			System.out.println("add-02");
+			nr.add(new Note("Nota2", "Questo � il testo della nota 2",myDate));
+			System.out.println("OK");
+			
+		} catch (InvalidKeyException e) {
+			System.out.println("ERR: " + e.getMessage());
+		}
+
+		try {
+			System.out.println("add-03");
+			GregorianCalendar gregorianCalendar1 = new GregorianCalendar(2017, GregorianCalendar.NOVEMBER, 12);
+			Date data1 = gregorianCalendar1.getTime();
+			nr.add(new Note("Nota3", "Questo � il testo della nota 3",data1));
+			System.out.println("OK");
+			System.out.println("OK");
+		} catch (InvalidKeyException e) {
+			System.out.println("ERR: " + e.getMessage());
+		}
+
+		try {
+			System.out.println("add-05");
+			nr.add(new Note("Nota5", "Questo � il testo della nota 5",myDate));
+			System.out.println("OK");
+		} catch (InvalidKeyException e) {
+			System.out.println("ERR: " + e.getMessage());
+		}
+		
+		try {
+			System.out.println("get-04");
+			System.out.println(nr.get("Nota4").toString());
+			System.out.println("OK");
+		} catch (InvalidKeyException e) {
+			System.out.println("ERR: " + e.getMessage());
+		}
+
+		
+		try {
+			System.out.println("add-03");
+			nr.add(new Note("Nota3", "Questo � il testo della nota 3",myDate));
+			System.out.println("OK");
+		} catch (InvalidKeyException e) {
+			System.out.println("ERR: " + e.getMessage());
+		}
+		
+		try {
+			System.out.println("get-01");
+			System.out.println(nr.get("Nota1").toString());
+			System.out.println("OK");
+		} catch (InvalidKeyException e) {
+			System.out.println("ERR: " + e.getMessage());
+		}
+		
+		System.out.println("update-01");
+		nr.update(new Note("Nota1", "Nuovo testo nota1", myDate));
+				
+		try {
+			System.out.println("get-01");
+			System.out.println(nr.get("Nota1").toString());
+			System.out.println("OK");
+		} catch (InvalidKeyException e) {
+			System.out.println("ERR: " + e.getMessage());
+		}
+		
+		System.out.println("Size: " + nr.size());
+		
+		try {
+			System.out.println("remove-01");
+			nr.remove("Nota1");
+			System.out.println("OK");
+		} catch (InvalidKeyException e) {
+			System.out.println("ERR: " + e.getMessage());
+		}
+		
+		System.out.println("Size: " + nr.size());
+		
+		try {
+			System.out.println("get-01");
+			System.out.println(nr.get("Nota1").toString());
+			System.out.println("OK");
+		} catch (InvalidKeyException e) {
+			System.out.println("ERR: " + e.getMessage());
+		}
+
+		try {
+			System.out.println("remove-08");
+			nr.remove("Nota8");
+			System.out.println("OK");
+		} catch (InvalidKeyException e) {
+			System.out.println("ERR: " + e.getMessage());
+		}
+		
+		System.out.println("Size: " + nr.size());
+
+		System.out.println("update-10");
+		nr.update(new Note("Nota10", "Testo nota 10", myDate));
+
+		System.out.println("Size: " + nr.size());
+		
+		try {
+			System.out.println("get-10");
+			System.out.println(nr.get("Nota10").toString());
+			System.out.println("OK");
+		} catch (InvalidKeyException e) {
+			System.out.println("ERR: " + e.getMessage());
+		}
+		
+		System.out.println("get-licences");
+		for (String t : nr.titles())
+				System.out.println("Titles: "+t);
+
+		Date d = new Date();
+		String notes[] = nr.getNotebyDate(d);
+		for (int i=0; i<notes.length; i++)
+			System.out.println(notes[i]);
+	
+	}
+}
